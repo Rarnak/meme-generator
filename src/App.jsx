b06ecbc7de8e91a0db16e16a5e2419b51e96029f
Rarnak <rarnakpariyar@gmail.com>
2026-09-24 11:41:11 +0545

    feat: complete initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
-export default function App(){
-  return(<h1>App Fwae</h1>)
+import Header from "./components/Header"
+import Main from "./components/Main"
+
+export default function App() {
+  return (<>
+    <Header />
+    <Main />
+  </>)
 }
